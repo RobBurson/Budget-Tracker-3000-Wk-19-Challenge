dbb5d106198fd547d4c8088c9791d0efeb95e75f
Rob <robburson07@gmail.com>
2022-02-05 08:45:05 -0600

    finished idb.js per the module's requirements, need to uncomment uploadTransaction

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -7,8 +7,8 @@ const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_exchange`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_exchange', { autoIncrement: true });
+    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_transaction', { autoIncrement: true });
   };
 
   // upon a successful 
@@ -16,10 +16,10 @@ request.onsuccess = function(event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
   
-    // check if app is online, if yes run uploadExchange() function to send all local db data to api
+    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
     if (navigator.onLine) {
       // we haven't created this yet, but we will soon, so let's comment it out for now
-      // uploadExchange();
+      // uploadTransaction();
     }
   };
   
@@ -31,24 +31,57 @@ request.onsuccess = function(event) {
   // This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['new_exchange'], 'readwrite');
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
   
-    // access the object store for `new_pizza`
-    const transactionObjectStore = transaction.objectStore('new_exchange');
+    // access the object store for `new_transaction`
+    const transactionObjectStore = transaction.objectStore('new_transaction');
   
     // add record to your store with add method
     transactionObjectStore.add(record);
   }
 
-  function uploadPizza() {
+  function uploadTransaction() {
     // open a transaction on your db
-    const transaction = db.transaction(['new_pizza'], 'readwrite');
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
   
     // access your object store
-    const pizzaObjectStore = transaction.objectStore('new_pizza');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
   
     // get all records from store and set to a variable
-    const getAll = pizzaObjectStore.getAll();
+    const getAll = transactionObjectStore.getAll();
   
-    // more to come...
+    // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transactions', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          // access the new_transaction object store
+          const transactionObjectStore = transaction.objectStore('new_transaction');
+          // clear all items in your store
+          transactionObjectStore.clear();
+
+          alert('All saved transactions have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
   }
+
+  // listen for app coming back online
+window.addEventListener('online', uploadTransaction);
